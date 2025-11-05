2767e334f5c17b8a3b808dae4006474706d3cd50
José Mauro <josemaurosilva600@gmail.com>
2025-11-04 21:19:13 -0300

    first commit

---
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/TsControlePatrimonioN2Application.java
@@ -0,0 +1,13 @@
+package com.josemauro.tscontrolepatrimonion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsControlePatrimonioN2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsControlePatrimonioN2Application.class, args);
+    }
+
+}
