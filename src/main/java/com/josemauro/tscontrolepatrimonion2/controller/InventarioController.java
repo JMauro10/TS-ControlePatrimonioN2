163db41a169ba6742327496181cfff631a519682
José Mauro <josemaurosilva600@gmail.com>
2025-11-08 17:03:43 -0300

    Mudanças na inventarioController para que os dados nao retorne null

--- src/main/java/com/josemauro/tscontrolepatrimonion2/controller/InventarioController.java
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/controller/InventarioController.java
@@ -1,7 +1,11 @@
 package com.josemauro.tscontrolepatrimonion2.controller;
 
 import com.josemauro.tscontrolepatrimonion2.entities.Baixa;
+import com.josemauro.tscontrolepatrimonion2.repository.LocalizacaoRepository;
+import com.josemauro.tscontrolepatrimonion2.repository.PatrimonioRepository;
+import com.josemauro.tscontrolepatrimonion2.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.josemauro.tscontrolepatrimonion2.entities.Inventario;
@@ -17,14 +21,44 @@ public class InventarioController {
     @Autowired
     private InventarioRepository inventarioRepository;
 
+    @Autowired
+    private PatrimonioRepository patrimonioRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private LocalizacaoRepository localizacaoRepository;
+
+
     @GetMapping
     public List<Inventario> listarInventario() {
         return inventarioRepository.findAll();
     }
 
     @PostMapping
-    public Inventario incluirInventario(@RequestBody Inventario inventario){
-        return inventarioRepository.save(inventario);
+    public ResponseEntity<Inventario> incluirInventario(@RequestBody Inventario inventario) {
+        // Carregando cada entidade relacionada antes de salvar para que os dados não retorne null
+        if (inventario.getPatrimonio() != null && inventario.getPatrimonio().getId() != null) {
+            inventario.setPatrimonio(
+                    patrimonioRepository.findById(inventario.getPatrimonio().getId()).orElse(null)
+            );
+        }
+
+        if (inventario.getLocalizacaoFisica() != null && inventario.getLocalizacaoFisica().getId() != null) {
+            inventario.setLocalizacaoFisica(
+                    localizacaoRepository.findById(inventario.getLocalizacaoFisica().getId()).orElse(null)
+            );
+        }
+
+        if (inventario.getAuditor() != null && inventario.getAuditor().getId() != null) {
+            inventario.setAuditor(
+                    pessoaRepository.findById(inventario.getAuditor().getId()).orElse(null)
+            );
+        }
+
+        Inventario inventarioSalvo = inventarioRepository.save(inventario);
+        return ResponseEntity.ok(inventarioSalvo);
     }
 
     @PutMapping
