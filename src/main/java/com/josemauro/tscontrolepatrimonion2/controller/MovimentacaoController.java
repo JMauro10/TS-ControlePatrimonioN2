1c87ae8b3e7784938d2f82a037533e9f92a81213
José Mauro <josemaurosilva600@gmail.com>
2025-11-07 23:29:12 -0300

    criação do MovimentacaoController

--- src/main/java/com/josemauro/tscontrolepatrimonion2/controller/MovimentacaoController.java
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/controller/MovimentacaoController.java
@@ -1,16 +1,39 @@
 package com.josemauro.tscontrolepatrimonion2.controller;
 
+import com.josemauro.tscontrolepatrimonion2.entities.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.josemauro.tscontrolepatrimonion2.entities.Movimentacao;
 import com.josemauro.tscontrolepatrimonion2.repository.MovimentacaoRepository;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/movimentacao")
 @CrossOrigin("*")
 public class MovimentacaoController {
     
     @Autowired
     private MovimentacaoRepository movimentacaoRepository;
+
+    @GetMapping
+    public List<Movimentacao> listarMovimentacao() {
+        return movimentacaoRepository.findAll();
+    }
+
+    @PostMapping
+    public Movimentacao incluirMovimentacao(@RequestBody Movimentacao movimentacao){
+        return movimentacaoRepository.save(movimentacao);
+    }
+
+    @PutMapping
+    public Movimentacao alterarMovimentacaoByID(@RequestBody Movimentacao movimentacao) {
+        return movimentacaoRepository.save(movimentacao);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarMovimentacaoById(@PathVariable Long id){
+        movimentacaoRepository.deleteById(id);
+    }
 }
