1e24fd30b2d83a952b6d43413ac05fcf86763237
José Mauro <josemaurosilva600@gmail.com>
2025-11-07 23:29:26 -0300

    criação do PatrimonioController

--- src/main/java/com/josemauro/tscontrolepatrimonion2/controller/PatrimonioController.java
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/controller/PatrimonioController.java
@@ -1,16 +1,40 @@
 package com.josemauro.tscontrolepatrimonion2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.josemauro.tscontrolepatrimonion2.entities.Movimentacao;
 import com.josemauro.tscontrolepatrimonion2.entities.Patrimonio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
 import com.josemauro.tscontrolepatrimonion2.repository.PatrimonioRepository;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/patrimonio")
 @CrossOrigin("*")
 public class PatrimonioController {
     
     @Autowired
     private PatrimonioRepository patrimonioRepository;
+
+    @GetMapping
+    public List<Patrimonio> listarPatrimonio() {
+        return patrimonioRepository.findAll();
+    }
+
+    @PostMapping
+    public Patrimonio incluirPatrimonio(@RequestBody Patrimonio patrimonio){
+        return patrimonioRepository.save(patrimonio);
+    }
+
+    @PutMapping
+    public Patrimonio alterarPatrimonioByID(@RequestBody Patrimonio patrimonio) {
+        return patrimonioRepository.save(patrimonio);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPatrimonioById(@PathVariable Long id){
+        patrimonioRepository.deleteById(id);
+    }
 }
