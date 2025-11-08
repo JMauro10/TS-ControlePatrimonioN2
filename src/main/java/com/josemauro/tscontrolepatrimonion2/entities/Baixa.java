e06011746e59be230afc0c187927f9184245926e
José Mauro <josemaurosilva600@gmail.com>
2025-11-08 00:18:06 -0300

    alteração nas entidades para que aparecer os dados no objeto movimentacao

--- src/main/java/com/josemauro/tscontrolepatrimonion2/entities/Baixa.java
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/entities/Baixa.java
@@ -5,15 +5,7 @@ import java.sql.Date;
 import com.josemauro.tscontrolepatrimonion2.enums.MotivoBaixaEnum;
 import com.josemauro.tscontrolepatrimonion2.enums.StatusInventarioEnum;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Baixa {
@@ -24,7 +16,7 @@ public class Baixa {
 
     // Um Patrimonio pode ter varias Baixas (historico), 
     // mas uma Baixa pertence a um unico Patrimonio.
-    @ManyToOne 
+    @ManyToOne (fetch = FetchType.EAGER)
     @JoinColumn(name = "patrimonio_id")
     private Patrimonio patrimonio;
 
@@ -40,7 +32,7 @@ public class Baixa {
     private StatusInventarioEnum status;
 
     // Uma Pessoa pode autorizar muitas Baixas.
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "autorizado_por_id") 
     private Pessoa autorizadoPor;
 
