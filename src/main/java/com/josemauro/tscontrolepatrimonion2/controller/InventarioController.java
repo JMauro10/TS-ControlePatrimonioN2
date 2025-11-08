d2f79173346517a0d0db1972127d88fe4221404e
José Mauro <josemaurosilva600@gmail.com>
2025-11-08 17:00:54 -0300

    Criação da inventarioController

--- src/main/java/com/josemauro/tscontrolepatrimonion2/controller/InventarioController.java
+++ src/main/java/com/josemauro/tscontrolepatrimonion2/controller/InventarioController.java
@@ -1,16 +1,39 @@
 package com.josemauro.tscontrolepatrimonion2.controller;
 
+import com.josemauro.tscontrolepatrimonion2.entities.Baixa;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.josemauro.tscontrolepatrimonion2.entities.Inventario;
 import com.josemauro.tscontrolepatrimonion2.repository.InventarioRepository;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/inventario")
 @CrossOrigin("*")
 public class InventarioController {
 
     @Autowired
     private InventarioRepository inventarioRepository;
+
+    @GetMapping
+    public List<Inventario> listarInventario() {
+        return inventarioRepository.findAll();
+    }
+
+    @PostMapping
+    public Inventario incluirInventario(@RequestBody Inventario inventario){
+        return inventarioRepository.save(inventario);
+    }
+
+    @PutMapping
+    public Inventario alterarInventarioByID(@RequestBody Inventario inventario) {
+        return inventarioRepository.save(inventario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarInventarioById(@PathVariable Long id){
+        inventarioRepository.deleteById(id);
+    }
 }
